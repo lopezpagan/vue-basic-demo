bae2351d646737cb2024cc591861fa7fb392d1b9
Tony <lopezpagan@gmail.com>
2016-12-02 01:26:40 -0400

    Initial Git

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#app',
+    
+    data: {
+        message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat error consequuntur dolorem maxime cupiditate repellendus ea expedita pariatur doloremque quod perferendis illo voluptates dicta rem at, quia ipsam minima temporibus!'
+    }
+})
